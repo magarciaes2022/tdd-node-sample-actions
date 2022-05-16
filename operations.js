44045f7bdeae9056f498517000903c7f0bdaa85c
Mª Ángeles García Escrig <ma.garciaescrig@edu.gva.es>
2022-05-16 16:26:22 +0200

    Update operations.js
    producto

--- operations.js
+++ operations.js
@@ -8,11 +8,13 @@ var resta = function suma (a, b) {
 	return a - b;	
 };
 
+
 /* modificacion 15.05.2022 */
 /* Función producto */
 var resta = function producto (a, b) {
 	producto a * b;	
-};
+}; 
+
 
 
 /* Exportación de funciones */
